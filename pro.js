2d3795f9ba5e0cf41c569a239643da244d33c421
amaroksc <53928868+amaroksc@users.noreply.github.com>
2021-01-10 05:39:35 -0400

    veamos

--- pro.js
+++ pro.js
@@ -1,36 +1,45 @@
-let cadena="pariente";
-console.log( cadena.length);
+/* let cadena = "pariente";
+console.log(cadena.length); */
 
 /* MÉTODOS EN JS */
 /**todos los metodos devuelven una cadena nueva, la cadena original no será modificada*/
-console.log("aquí volvemos en mayuscula todo: "+cadena+" a: "+cadena.toUpperCase());
-let cadenamayuscula=cadena.toUpperCase();//aqui     guardamos la mayuscula en 1 variable
- function functionstring1() {
-     var a,texto;     
-     a=document.getElementById("imput1").value;
-     
+// console.log("aquí volvemos en mayuscula todo: " + cadena + " a: " + cadena.toUpperCase());
+// let cadenamayuscula = cadena.toUpperCase(); //aqui     guardamos la mayuscula en 1 variable
 
 
- }
+function functionstring1() {
+    var a, texto;
+    a = document.getElementById("imput1").value;
+    if (isNaN(a) || a < 1 || a > 10) {
+        texto = "input not valid";
+    } else {
+        texto = "perfect";
+    }
+    document.getElementById("reemplazado1").innerHTML = texto;
+
+
+}
 /** EMPEZANDO DARK MODE */
-        function changeColor(newColor){
-            var elem=document.getElementById('for');
-            elem.style.color=newColor;
-        }
-        function changecolorcorto(){
-document.getElementById("cambia-color").style.backgroundColor="rgb(145, 0, 255)";
-document.getElementById("cambia-color").style.color="rgb(255, 255, 255)";
-document.get
-        }
-        function functiondarklightmode(){
-            var palanca=document.body;
-            y=palanca.classList.toggle("dark-mode");
-        
-        }
+function changeColor(newColor) {
+    var elem = document.getElementById('for');
+    elem.style.color = newColor;
+}
 
-       
-        /*document.getElementById("span-darkmode-to-lightmode").innerHTML="light mode" */
-        /*function textlightmode(){
+function changecolorcorto() {
+    document.getElementById("cambia-color").style.backgroundColor = "rgb(145, 0, 255)";
+    document.getElementById("cambia-color").style.color = "rgb(255, 255, 255)";
+    document.get
+}
+
+function functiondarklightmode() {
+    var palanca = document.body;
+    y = palanca.classList.toggle("dark-mode");
+
+}
+
+
+/*document.getElementById("span-darkmode-to-lightmode").innerHTML="light mode" */
+/*function textlightmode(){
             var x=document.getElementById("button-dar-light");
             if(x.display==="dark mode"){
                 x.display="light mode";
@@ -43,4 +52,4 @@ document.get
                 }
             }
 */
-    /** TERMINANDO DARK MODE */  
+/** TERMINANDO DARK MODE */
