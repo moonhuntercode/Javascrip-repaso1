f2d47455fa37d70166179a460d92f8c4a4be7126
moonhunter <vdcloayza@gmail.com>
2021-03-12 23:48:30 -0400

    vamos mejorando

---
+++ js/Arrray.js
@@ -0,0 +1,12 @@
+const arra1=[1,2,3,4]
+const arra2=new Array(80,20)
+const users=['Alan','Peter','Ben','John']
+
+const any=['Alan',3,null,undefined,
+false,{name:'puto',age:30},new Date()]
+
+
+console.log(arra2)
+console.log(arra1)
+console.log(users)
+console.log(any)
