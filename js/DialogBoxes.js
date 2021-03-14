b9092207018a4bd7538167aa3c92c175df642bae
moonhunter <vdcloayza@gmail.com>
2021-03-13 23:45:42 -0400

    uso de dialogfunction3

--- js/DialogBoxes.js
+++ js/DialogBoxes.js
@@ -1,3 +1,11 @@
 function dialogfunction(){
-    
+    let retval=confirm("confirmado")
+    if (retval==true) {
+        document.write("confirmadisimo")
+        return true
+    }
+    else{
+document.write("negadisimo")
+return false
+    }
 }
