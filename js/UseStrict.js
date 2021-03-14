110d0bc1ce4f7a134c668bcf45fa7034fb608024
moonhunter <vdcloayza@gmail.com>
2021-03-13 23:19:44 -0400

    uso de use strict 2

--- js/UseStrict.js
+++ js/UseStrict.js
@@ -1,4 +1,4 @@
-// "use strict"
+"use strict"
 /* En javaScript gracias a EcmaScript
 el uso de "use strict" te ayuda
 a forzarte a usar buenas prácticas
@@ -8,7 +8,14 @@ depende de cada uno.
 siempre se usa sí o sí al principio
 del archivo en cualquiera que lo
 apliques, siempre al principio
-se debe usar
+se debe usar.
+Si yo no uso "use strict" el siguiente
+ejemplo de abajo funciona sin problemas,
+ya que no me pedirá definir la variable
  */
-name=pablo
-console.log(name)
+
+names="pablo"
+console.log(names)
+/* Pero si uso "use strict" te van 
+a obligar a definir la variable
+y usar buenas convenciones */
