63637e9d2041deb774a2fab6886cc55cf3032435
moonhunter <vdcloayza@gmail.com>
2021-03-13 23:15:49 -0400

    use strict y su uso

---
+++ js/UseStrict.js
@@ -0,0 +1,14 @@
+// "use strict"
+/* En javaScript gracias a EcmaScript
+el uso de "use strict" te ayuda
+a forzarte a usar buenas prácticas
+en tu código , archivos utilizados
+aunque es opcional, porque es 
+depende de cada uno.
+siempre se usa sí o sí al principio
+del archivo en cualquiera que lo
+apliques, siempre al principio
+se debe usar
+ */
+name=pablo
+console.log(name)
