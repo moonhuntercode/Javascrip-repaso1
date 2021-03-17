5558f666647f0f89c5d4ad2a5e09268ae6862105
moonhunter <vdcloayza@gmail.com>
2021-03-16 23:29:48 -0400

    Funciones dentro de funciones parte2

--- js/Func-inside-Func.js
+++ js/Func-inside-Func.js
@@ -2,7 +2,7 @@
 var a1=prompt("Dame una",'uno');
 var a2=prompt('Dame otra','otra');
 function MostrarConsola(n1,n2){
-    console.log(n1+ " "+n2);
+    console.log(n1+" "+n2);
 }
 function EnDocument(c1,c2){
     document.write(c1+" "+c2);
@@ -15,4 +15,4 @@ MostrarConsola(a1,a2);
         EnDocument(a1,a2)
     }
 }
-Mostrar(a1,a2,true);
+Mostrar(a1,a2,false);
