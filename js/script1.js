d1c39d9b3f103ae87019a3e99a0bd59205257a0d
moonhunter <vdcloayza@gmail.com>
2021-04-15 18:54:12 -0400

    2 temas añadidos con éxito

---
+++ js/script1.js
@@ -0,0 +1,10 @@
+import APP from './component1.js';
+import { banana as platano, orange } from './component2.js';
+console.log('export banana ', platano);
+console.log('export orange ', orange);
+console.log('export default ', APP);
+/* o también podemos importar todo
+en un solo objeto como abajo */
+import * as fruits from './component2.js'
+console.log('export * banana', fruits.banana);
+console.log('export * orange', fruits.orange);
